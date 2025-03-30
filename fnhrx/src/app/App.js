b0d909ff21122e2fdbbf27aa5ff81b78f3452d63
RynSingleton <kamrynsingletonyt@gmail.com>
2025-03-30 02:28:48 -0500

    working on tailwind

---
+++ fnhrx/src/app/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome to My React Server!</h1>
+      <p>Your server is up and running on port 3000.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
